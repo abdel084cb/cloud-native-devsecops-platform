1d9cb03bdc9ea52f710166757fb4933f66669e73
abdel <abdel@example.com>
2026-08-26 17:49:25 +0200

    feat: externalize application configuration and reorganize files

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+set -euo pipefail
+
 docker run -d \
   --name cloud-native-devsecops-api \
   -p 127.0.0.1:8001:8000 \
+  -e LOG_LEVEL=INFO \
+  -e ENVIRONMENT=local \
+  -e UVICORN_PORT=8000 \
   --read-only \
   --tmpfs /tmp \
   --cap-drop ALL \
