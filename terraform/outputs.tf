004b0b81c4138f49a3d6df3e01e1df9611cffbf3
abdel <abdel@example.com>
2026-09-04 20:50:58 +0200

    feat: updated output.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,24 @@ output "ecr_repository_url" {
   description = "URL of the ECR repository"
   value       = aws_ecr_repository.repo.repository_url
 }
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_b.id
+  ]
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id
+  ]
+}
